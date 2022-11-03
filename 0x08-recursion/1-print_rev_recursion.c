7a9b50973668eeef17b96d048acb25d6eddd3e0b
Ogeben1 <ogeben92@gmail.com>
2022-11-03 07:52:06 -0700

    printreverserecursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - prints a string in reverse.
- * @s: string
- * Return: no return.
- */
+* _print_rev_recursion - prints a string in reve
+* @s: string
+* Return: no return.
+*/
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s)
+{
+_print_rev_recursion(s + 1)
+_putchar(*s)
+}
+
 }
