411c8ae8b3cfbb086ddb4e2f972570a4d8853b88
Ogeben1 <ogeben92@gmail.com>
2022-11-28 13:41:08 -0800

    learning c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
-#include "function_pointers.h" 
-  
- /** 
-  * print_name - prints a name 
-  * @name: name to be printed 
-  * @f: callback function that prints name 
-  */ 
- void print_name(char *name, void (*f)(char *)) 
- { 
-         if (!f || !name) 
-                 return; 
-         (*f)(name); 
- }
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,19 @@
-#include "function_pointers.h" 
- /** 
-  * array_iterator - ___ 
-  * @array: ___ 
-  * @size: ___ 
-  * @action: ___ 
-  * 
-  * Return: ___ 
-  */ 
- void array_iterator(int *array, size_t size, void (*action)(int)) 
- { 
-         unsigned int i = 0; 
-  
-         if (array != NULL && action != NULL && size > 0) 
-         { 
-                 while (i < size) 
-                 { 
-                         action(array[i]); 
-                         i++; 
-                 } 
-         } 
- }
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
